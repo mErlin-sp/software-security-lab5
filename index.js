c829e52ee92224b69905dcfd05d5043263049a86
Oleksandr <merlin.ua.1@gmail.com>
2023-11-12 22:38:08 +0200

    add token refresh when expire

--- index.js
+++ index.js
@@ -19,7 +19,48 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-    res.render('index', {username: req.session.username})
+
+    try {
+        console.log(`Expires in: ${req.session.expiresIn - new Date().getTime()}`)
+    } catch (err) {
+    }
+    if (req.session.expiresIn && (req.session.expiresIn - new Date().getTime()) < 10000) {
+        console.log('Refresh Token')
+        const options = {
+            method: 'POST',
+            url: 'https://dev-00wdj4huibiu7y8p.us.auth0.com/oauth/token',
+            headers: {'content-type': 'application/x-www-form-urlencoded'},
+            form:
+                {
+                    grant_type: 'refresh_token',
+                    client_id: 'bWUNFvT10QJBoWqZn7b4MYAfwMFS8q8C',
+                    client_secret: 'IaAds7SUSktrybzfIH0Rp3a8WmwdiGAX5sGq9HU2JV0RUPWw1gR5nCqaWAvw44ug',
+                    refresh_token: req.session.refreshToken
+                }
+        };
+
+        request(options, function (error, response, resBody) {
+            if (error) {
+                console.error(error)
+                res.status(500).send(error)
+                return
+            }
+
+            resBody = JSON.parse(resBody)
+            console.log(`resBody: ${resBody}`)
+
+            if (resBody['error']) {
+                res.status(500).send(resBody)
+                return
+            }
+
+            req.session.accessToken = resBody['access_token']
+            req.session.refreshToken = resBody['refresh_token']
+            req.session.expiresIn = new Date().getTime() + (parseInt(resBody['expires_in']) * 1000)
+            console.log(`Expires in: ${new Date(req.session.expiresIn)}`)
+        });
+    }
+    res.render('index', {login: req.session.login})
 })
 
 app.get('/logout', (req, res) => {
@@ -30,7 +71,7 @@ app.get('/logout', (req, res) => {
 
 app.post('/api/login', (req, res) => {
     const {login, password} = req.body;
-    console.log(login + '  ' + password)
+    console.log(`Login: ${login} ${password}`)
 
     const options = {
         method: 'POST',
@@ -48,27 +89,29 @@ app.post('/api/login', (req, res) => {
         }
     };
 
-    request(options, function (error, response, body) {
+    request(options, function (error, response, resBody) {
         if (error) {
-            console.log('error')
-            res.status(501).send(error)
-        } else {
-            body = JSON.parse(body)
-            console.log(body)
-
-            if (body['error']) {
-                console.log('error')
-                res.status(501).send(body)
-            } else {
-                req.session.accessToken = body['access_token']
-                req.session.username = login;
-                req.session.login = login;
-
-                res.json({token: body['access_token']});
-            }
+            console.error(error)
+            res.status(500).send(error)
+            return
         }
 
+        resBody = JSON.parse(resBody)
+        console.log(`resBody: ${resBody}`)
 
+        if (resBody['error']) {
+            console.error(error)
+            res.status(500).send(resBody)
+            return;
+        }
+
+        req.session.accessToken = resBody['access_token']
+        req.session.refreshToken = resBody['refresh_token']
+        req.session.expiresIn = new Date().getTime() + (parseInt(resBody['expires_in']) * 1000)
+        req.session.login = login;
+        console.log(`Expires in: ${new Date(req.session.expiresIn)}`)
+
+        res.json({token: resBody['access_token']});
     });
 
 })
