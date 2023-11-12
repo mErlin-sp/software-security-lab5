70a08e302e16edc63d62191a80188c0d56568e74
Oleksandr <merlin.ua.1@gmail.com>
2023-11-12 23:41:00 +0200

    add user create

--- index.js
+++ index.js
@@ -18,6 +18,8 @@ app.set('view engine', 'pug')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 
     try {
@@ -61,6 +63,15 @@ app.get('/', (req, res) => {
         });
     }
     res.render('index', {login: req.session.login})
+})
+
+app.get('/create-user', (req, res) => {
+
+    if (req.session.expiresIn && (req.session.expiresIn - new Date().getTime()) < 10000) {
+        res.redirect('/')
+        return
+    }
+    res.render('create-user', {})
 })
 
 app.get('/logout', (req, res) => {
@@ -113,6 +124,77 @@ app.post('/api/login', (req, res) => {
 
         res.json({token: resBody['access_token']});
     });
+
+})
+
+app.post('/api/create-user', (req, res) => {
+    const options = {
+        method: 'POST',
+        url: 'https://dev-00wdj4huibiu7y8p.us.auth0.com/oauth/token',
+        headers: {'content-type': 'application/x-www-form-urlencoded'},
+        form: {
+            audience: "https://dev-00wdj4huibiu7y8p.us.auth0.com/api/v2/",
+            grant_type: "client_credentials",
+            client_id: "4aeeF2vTXRHaLVcrvYkZA5u8swvBFzJz",
+            client_secret: "IQTr49MvuDUS536QV3xuDO26Aoe74SKAuECXcNGISvCX5pDIemCAVRCYYmkcb6de",
+        }
+    };
+
+    request(options, function (error, response, body) {
+        if (error) {
+            console.error(error)
+            res.status(500).send(error)
+            return;
+        }
+
+        body = JSON.parse(body)
+        if (body['error']) {
+            console.error(body)
+            res.status(500).send(body)
+            return;
+        }
+
+        const {email, name, password} = req.body;
+        console.log(`Email: ${email} Name: ${name} Password: ${password}`)
+        console.log(`Bearer ${body['access_token']}`)
+
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        myHeaders.append("Accept", "application/json");
+        myHeaders.append("Authorization", `Bearer ${body['access_token']}`);
+
+        const raw = JSON.stringify({
+            "email": email,
+            "name": name,
+            "password": password,
+            "connection": "my-connection",
+        });
+
+        const requestOptions = {
+            method: 'POST',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        fetch("https://dev-00wdj4huibiu7y8p.us.auth0.com/api/v2/users", requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(`Create User: ${result}`)
+
+                result = JSON.parse(result)
+                if (result['error']) {
+                    res.status(500).send(result)
+                    return;
+                }
+                res.status(200).send(result)
+            })
+            .catch(error => {
+                console.error(error)
+                res.status(500).send(error)
+            });
+    });
+
 
 })
 
